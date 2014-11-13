f5151a91db6b06ff7f7f186a153d9770ec30d018
vic.herrero <vic.herrero89@gmail.com>
2014-11-13 16:54:27 +0000

    Neo4j on RR

--- frontend/app/controllers/home_controller.rb
+++ frontend/app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
+    Neo4j::Session.open(:server_db)
+#    Neo4j::Transaction.run do
+#      me   = Neo4j::Node.create(:name => 'Me',   :age => 31)
+#      bob  = Neo4j::Node.create(:name => 'Bob',  :age => 29)
+#      mark = Neo4j::Node.create(:name => 'Mark', :age => 34)
+#      mary = Neo4j::Node.create(:name => 'Mary', :age => 32)
+#      john = Neo4j::Node.create(:name => 'John', :age => 33)
+#      andy = Neo4j::Node.create(:name => 'Andy', :age => 31)
+      
+#      me.create_rel(:friends, bob, :value => 3)
+#      bob.create_rel(:friends, mark)
+#      mark.create_rel(:friends, mary)
+#      mary.create_rel(:friends, john)
+#      john.create_rel(:friends, andy)
+#   end
+    @query = Neo4j::Session.query.match('n-[r]->m').pluck('n.name,type(r),m.value')
   end
 end
--- frontend/config/application.rb
+++ frontend/config/application.rb
@@ -19,5 +19,8 @@ module Myapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Neo4j configuration
+    config.neo4j.session_path = 'http://localhost:7474/db/data'
   end
 end
